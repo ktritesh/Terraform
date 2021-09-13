db6532859eb9c3a5256db700c0e9db225312d62e
Ritesh Kumar Tiwari <82252410+ktritesh@users.noreply.github.com>
2021-09-13 10:46:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@
 provider "aws" {
   region = "ap-south-1"
   profile = "root"
-  access_key = "AKIATLITCVFW2RCOSMH7"
-  secret_key = "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"
+  access_key = 
+  secret_key = 
 }
